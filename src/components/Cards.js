61a802dca7d352b8dcfccf4f987f8c94d531f4a3
nivorako <nivo.rakoto@yahoo.fr>
2023-02-20 16:55:44 +0100

    try to fetch data in cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,19 +1,47 @@
+
+import { useEffect, useState } from "react"
+
 export default function Cards(){
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch("..src/datas/housing.json")
+            .then((res) => {
+                if(!res.ok) {
+                    throw new Error(`This is an HTTP error: The status is ${res.status}`)
+                }
+                console.log("actualData :", res.json())
+                return res.json()
+            })
+            .then((actualData) => {
+                console.log("actualData :", actualData)
+                setData(actualData)
+                setError(null)
+            })
+            .catch((err) => {
+                setError(err.message)
+                setData(null)
+            })
+            .finally(() => {
+                setLoading(false)
+            })
+    }, [])
     return(
         <section className="cards" >
-            <div className="card">a</div>
-            <div className="card">bmploikju</div>
-            <div className="card">c</div>
-            <div className="card">d</div>
-            <div className="card">e</div>
-            <div className="card">f</div>
-            <div className="card">a</div>
-            <div className="card">bmploikju</div>
-            <div className="card">c</div>
-            <div className="card">d</div>
-            <div className="card">e</div>
-            <div className="card">f</div>
-            
+            {loading && <div>A moment please...</div>}
+            {error && (
+                <div>{`There is a problem fetching the post data - ${error}`}</div>
+            )}
+            <ul>
+                {data &&
+                data.map(({ id, title }) => (
+                    <li key={id}>
+                    <h3>{title}</h3>
+                    </li>
+                ))}
+            </ul>
         </section>
     )
 }
