fbc7f70943dc02d8181c03129a994f7249cea5b7
nivorako <nivo.rakoto@yahoo.fr>
2023-02-27 15:07:07 +0100

    make housing__detailsHeader responsive

--- src/components/Carrousel.js
+++ src/components/Carrousel.js
@@ -3,21 +3,22 @@ import arrow from "../assets/arrow.png"
 import { useEffect, useState } from "react"
 
 export default function Carrousel({imgSrc,alt}){
-    const [index, slideindex] = useState(1)
+    const [index, slideindex] = useState(0)
 
     useEffect(() => {
         showSlide(index)
     }, [])
 
     function showSlide(n){
+        
         const items = document.getElementsByClassName("carrousel__item")
         const length = items.length
-        if(n > items.length) n = 1
-        if(n < 1) n = length
+        if(n > length) slideindex(index)
+        if(n === 0) slideindex(length)
         for(let i = 0; i < length; i++){
             items[i].style.display = "none"
         }
-        items[n-1].style.display = "block"
+        items[n].style.display = "block"
     }
     function currentSlide(n) {
         const dots = document.getElementsByClassName("carrousel__dot")
@@ -29,6 +30,9 @@ export default function Carrousel({imgSrc,alt}){
         showSlide(n)
         
     }
+    function handleOnClickRightA(){
+        console.log('aie aie')
+    }
     return(
         <section className="carrousel">
             <ul className="carrousel__items">
@@ -37,8 +41,20 @@ export default function Carrousel({imgSrc,alt}){
                         ? (imgSrc.map((src, index) => (
                             <li key={index} className="carrousel__item">
                                 <img src={src} alt={alt} className="carrousel__img"/>
-                                <img src={arrow} alt="fleche droite" className="carrousel__rightA"></img>
-                                <img src={arrow} alt="fleche gauche" className="carrousel__leftA"></img>
+                                
+                                    <img 
+                                        src={arrow} 
+                                        alt="fleche droite" 
+                                        className="carrousel__rightA" 
+                                        onClick={() => handleOnClickRightA()}  
+                                    />
+                                
+                                
+                                <img 
+                                    src={arrow} 
+                                    alt="fleche gauche" 
+                                    className="carrousel__leftA"
+                                />
                             </li>
                         )))
                         :   (
