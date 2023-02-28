9ebc9f7d1af4bfe9bffd9a20a93fcc8f8d64662d
nivorako <nivo.rakoto@yahoo.fr>
2023-02-28 02:54:10 +0100

    slider arrow are working

--- src/components/Carrousel.js
+++ src/components/Carrousel.js
@@ -1,25 +1,24 @@
 import arrow from "../assets/arrow.png"
 
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 
 export default function Carrousel({imgSrc,alt}){
-    const [index, slideindex] = useState(0)
-
+   
+    let index = 0
     useEffect(() => {
         showSlide(index)
-    }, [])
+    })
 
     function showSlide(n){
         
         const items = document.getElementsByClassName("carrousel__item")
         const length = items.length
-        if(n > length) slideindex(index)
-        if(n === 0) slideindex(length)
         for(let i = 0; i < length; i++){
             items[i].style.display = "none"
         }
         items[n].style.display = "block"
     }
+
     function currentSlide(n) {
         const dots = document.getElementsByClassName("carrousel__dot")
         const l = dots.length
@@ -27,12 +26,38 @@ export default function Carrousel({imgSrc,alt}){
             dots[i].classList.remove("active")
         }
         dots[n].classList.add("active")
-        showSlide(n)
+        showSlide(n)  
+    }
+    function handleOnClickRightA(n){
+       
+        const items = document.getElementsByClassName("carrousel__item")
+        const length = items.length
         
+        for(let i = 0; i < length; i++){
+            items[i].style.display = "none"
+        }
+        if(n  === length - 1) {
+            n = 0
+        }
+        
+        items[n + 1].style.display = "block"
     }
-    function handleOnClickRightA(){
-        console.log('aie aie')
+
+    function handleOnClickLeftA(n){
+        const items = document.getElementsByClassName("carrousel__item")
+        const length = items.length
+        
+        for(let i = 0; i < length; i++){
+            items[i].style.display = "none"
+        }
+        if(n  === 0) {
+            console.log('n < length : ', n)
+            n = length
+        }
+        
+        items[n - 1].style.display = "block"
     }
+
     return(
         <section className="carrousel">
             <ul className="carrousel__items">
@@ -42,18 +67,18 @@ export default function Carrousel({imgSrc,alt}){
                             <li key={index} className="carrousel__item">
                                 <img src={src} alt={alt} className="carrousel__img"/>
                                 
-                                    <img 
-                                        src={arrow} 
-                                        alt="fleche droite" 
-                                        className="carrousel__rightA" 
-                                        onClick={() => handleOnClickRightA()}  
-                                    />
-                                
+                                <img 
+                                    src={arrow} 
+                                    alt="fleche droite" 
+                                    className="carrousel__rightA" 
+                                    onClick={() => handleOnClickRightA(index)}  
+                                />
                                 
                                 <img 
                                     src={arrow} 
                                     alt="fleche gauche" 
                                     className="carrousel__leftA"
+                                    onClick={() => handleOnClickLeftA(index)} 
                                 />
                             </li>
                         )))
