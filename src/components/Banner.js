29df4b1b9afafc5cbb40d4e9440e520234274193
nivorako <nivo.rakoto@yahoo.fr>
2023-03-17 16:51:46 +0100

    fix title words break in banner home

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,12 @@
 export default function Banner({imgSrc, title, altTxt}){
+    console.log("title1: ", title.split("").splice(0, 10).join(""))
+    console.log("title2: ", title.split("").splice(10).join(""))
     return(
         <section className="banner">
             <img src={imgSrc} alt={altTxt} />
             <h1> 
-                {title}
+                <p>{title.split("").splice(0, 10).join("")} </p>
+                <p>{title.split("").splice(10).join("")} </p>
             </h1>
         </section>
     )
