2394dc86ff3fc8a59a679ea711acca1b18920644
nivorako <nivo.rakoto@yahoo.fr>
2023-03-17 14:55:55 +0100

    fix stuff

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,7 +2,9 @@ export default function Banner({imgSrc, title, altTxt}){
     return(
         <section className="banner">
             <img src={imgSrc} alt={altTxt} />
-            <h1> {title} </h1>
+            <h1> 
+                {title}
+            </h1>
         </section>
     )
 }
